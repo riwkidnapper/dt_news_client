93b0cdc211718fec71be81c795a5f4a56d48d7f1
riwkidnapper <riw.kidnapper@gmail.com>
2019-08-16 11:28:19 +0700

    update downloadnews

--- src/views/downloadnews.js
+++ src/views/downloadnews.js
@@ -126,6 +126,102 @@ class downloadnews extends Component {
                       </div>
                     </CardFooter>
                   </Card>
+                </Col>{" "}
+                <Col lg="4" md="6" sm="6">
+                  <Card className="card-stats">
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://www.gogoprint.co.th/media/wysiwyg/opening-images_41.jpg"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <Row>
+                        <Col md="4" xs="5">
+                          <div className="icon-big text-center icon-warning">
+                            <i className="nc-icon nc-vector text-danger" />
+                          </div>
+                        </Col>
+                        <Col md="8" xs="7">
+                          <div className="numbers">
+                            <p className="card-category">Errors</p>
+                            <CardTitle tag="p">23</CardTitle>
+                            <p />
+                          </div>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                    <CardFooter>
+                      <hr />
+                      <div className="stats">
+                        <i className="far fa-clock" /> In the last hour
+                      </div>
+                    </CardFooter>
+                  </Card>
+                </Col>
+                <Col lg="4" md="6" sm="6">
+                  <Card className="card-stats">
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://www.gogoprint.co.th/media/wysiwyg/opening-images_41.jpg"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <Row>
+                        <Col md="4" xs="5">
+                          <div className="icon-big text-center icon-warning">
+                            <i className="nc-icon nc-vector text-danger" />
+                          </div>
+                        </Col>
+                        <Col md="8" xs="7">
+                          <div className="numbers">
+                            <p className="card-category">Errors</p>
+                            <CardTitle tag="p">23</CardTitle>
+                            <p />
+                          </div>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                    <CardFooter>
+                      <hr />
+                      <div className="stats">
+                        <i className="far fa-clock" /> In the last hour
+                      </div>
+                    </CardFooter>
+                  </Card>
+                </Col>
+                <Col lg="4" md="6" sm="6">
+                  <Card className="card-stats">
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://www.gogoprint.co.th/media/wysiwyg/opening-images_41.jpg"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <Row>
+                        <Col md="4" xs="5">
+                          <div className="icon-big text-center icon-warning">
+                            <i className="nc-icon nc-vector text-danger" />
+                          </div>
+                        </Col>
+                        <Col md="8" xs="7">
+                          <div className="numbers">
+                            <p className="card-category">Errors</p>
+                            <CardTitle tag="p">23</CardTitle>
+                            <p />
+                          </div>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                    <CardFooter>
+                      <hr />
+                      <div className="stats">
+                        <i className="far fa-clock" /> In the last hour
+                      </div>
+                    </CardFooter>
+                  </Card>
                 </Col>
               </Row>
             </Col>
